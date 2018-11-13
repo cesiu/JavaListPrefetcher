28eef16746f25ef363f339c95cd6cb9ecbeb4733
Christopher Siu <christopher@siu.sh>
2018-11-13 11:35:11 -0800

    Removed classfiles; little more work with hooking into parsing.

--- src/csc515/plugin/TestPlugin.java
+++ src/csc515/plugin/TestPlugin.java
@@ -7,6 +7,8 @@ package csc515.plugin;
 
 import com.sun.source.util.Plugin;
 import com.sun.source.util.JavacTask;
+import com.sun.source.util.TaskListener;
+import com.sun.source.util.TaskEvent;
 
 public class TestPlugin implements Plugin
 {
@@ -19,6 +21,20 @@ public class TestPlugin implements Plugin
 
     @Override
     public void init(JavacTask task, String... args) {
-        System.err.println("Now THIS is podracing!");
+        System.err.println("Now THIS is podracing!\n");
+
+        task.addTaskListener(new TaskListener() {
+            @Override
+            public void started(TaskEvent e) {}
+
+            @Override
+            public void finished(TaskEvent e) {
+                if (e.getKind() == TaskEvent.Kind.PARSE) {
+                    System.err.println(
+                            "Finished parsing " + e.getSourceFile() + ":\n" +
+                            e.getCompilationUnit());
+                }
+            }
+        });
     }
 }
